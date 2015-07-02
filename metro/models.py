ae73be69775d8444f653eabca3d18ef519ebe0ce
Abdul Muneer <abdulmuneer@gmail.com>
2015-07-02 23:35:00 +0530

    minimum viable product ;)

--- metro/models.py
+++ metro/models.py
@@ -3,6 +3,10 @@ from sqlalchemy import (
     Index,
     Integer,
     Text,
+    String,
+    ForeignKey,
+    UniqueConstraint,
+    PrimaryKeyConstraint
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -24,4 +28,31 @@ class MyModel(Base):
     name = Column(Text)
     value = Column(Integer)
 
+class Station(Base):
+    __tablename__ = 'station'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(256))
+
+class Phase(Base):
+    '''
+    denotes multiple phases/routes of metro
+    '''
+    __tablename__ = 'phase'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(256))
+
+class Route(Base):
+    '''
+    denotes series of stations along different phases of metro.
+    '''
+    __tablename__ = 'route'
+    id = Column(Integer, primary_key=True)
+    phase_id = Column(Integer, ForeignKey('phase.id'))
+    station_id = Column(Integer, ForeignKey('station.id'))
+    order = Column(Integer, nullable=False, autoincrement=False)
+    __table_args__ = (UniqueConstraint(phase_id, station_id, order),)
+
+
 Index('my_index', MyModel.name, unique=True, mysql_length=255)
+
+
--- metro/scripts/initializedb.py
+++ metro/scripts/initializedb.py
@@ -13,10 +13,32 @@ from pyramid.scripts.common import parse_vars
 
 from ..models import (
     DBSession,
-    MyModel,
+    Station,
+    Phase,
+    Route,
     Base,
     )
 
+original_stations = ["CST",
+"Masjid",
+"Sandhurst Road",
+"Byculla",
+"Chinchpokli",
+"Currey Road",
+"Parel",
+"Dadar",
+"Matunga",
+"Sion",
+"Kurla",
+"Vidyavihar",
+"Ghatkopar",
+"Vikhroli",
+"Kanjurmarg",
+"Bhandup",
+"Nahur",
+"Mulund",
+"Thane",
+"Kalyan"]
 
 def usage(argv):
     cmd = os.path.basename(argv[0])
@@ -32,9 +54,27 @@ def main(argv=sys.argv):
     options = parse_vars(argv[2:])
     setup_logging(config_uri)
     settings = get_appsettings(config_uri, options=options)
+    add_phase(settings)
+    add_station_routes(settings)
+
+def add_phase(settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
     with transaction.manager:
-        model = MyModel(name='one', value=1)
-        DBSession.add(model)
+        phase = Phase(id=1, name='phase1')
+        DBSession.add(phase)
+
+
+def add_station_routes(settings):
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+    Base.metadata.create_all(engine)
+
+    with transaction.manager:
+        stations = [Station(id=station_order, name= station_name)
+                    for (station_order, station_name) in enumerate(original_stations,1)]
+        route = [Route(phase_id=1, station_id=station.id, order=station_order)
+                 for (station_order, station) in enumerate(stations,1)]
+        [DBSession.add(station) for station in stations]
+        [DBSession.add(stop) for stop in route]
